6f860c1d2f5c0c8b16fd7b84c327369825683712
meryam <meryam.chouaiti@smartegy.ca>
2022-06-15 14:19:54 +0200

    Module Vehicule

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -3,12 +3,18 @@ export class Car {
   year: string | any ;
   model: string | any ;
   type: string | any ;
+  portes: string | any ;
+  siege: string | any ;
+  carburant: string | any ;
+  transmission: string | any ;
   options : any[] ;
   Price_D : number | any ;
   Price_H : number | any ;
   matricule : string | any ;
   user_id : number | any ;
-  galleris : any[] ;
+  gallerie : any[] ;
+  galleries : any[] ;
+  photo : string ;
 
 
 
