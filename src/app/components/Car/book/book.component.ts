4d54658c6d69eb7f53ba2d9a706515a77c1603d6
Jebarif <98496405+Jebarif@users.noreply.github.com>
2022-06-24 16:40:39 +0200

    verif & book part1

--- src/app/components/Car/book/book.component.ts
+++ src/app/components/Car/book/book.component.ts
@@ -10,9 +10,16 @@ export class BookComponent implements OnInit {
   res: any = null ;
   date : Date = new Date();
   value4 : any ;
+  totalprice:any = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
+ prix()
+ {
+  console.log("ok");
+ }
 }
